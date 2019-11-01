d7c4da907a17a8abc6568d6b6b14b0f3c5d471e4
Olufisayo Akinmoju <hfisk000@email.cpcc.edu>
2019-11-01 17:28:29 +0100

    display boomarks from async storage

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { ScrollView, RefreshControl } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage';
+import Article from './Article';
 
 export default class Bookmarks extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      articles: {}
+    }
+    this.getData()
   }
+
+
+  getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@news-app-bookmarks')
+      if (value !== null) {
+        // value previously stored
+        const result = JSON.parse(value)
+        this.setState({
+          articles: result
+        })
+      }
+    } catch (e) {
+      // error reading value
+      console.log(e)
+    }
+
+  }
+
+
+
   render() {
+    const { bookmarks } = this.state.articles
     return (
-      <View>
-        <Text>Bookmarks Component</Text>
-      </View>
+      <ScrollView>
+        {
+          bookmarks && bookmarks.map((bookmark, index) => (
+            <Article key={index} article={bookmark} style={{ elevation: 10 }} navigation={this.props.navigation} />
+          ))
+        }
+      </ScrollView>
     )
   }
 }
--- src/store/rootStore.js
+++ src/store/rootStore.js
@@ -1,8 +1,20 @@
+import { create } from 'mobx-persist'
+import AsyncStorage from '@react-native-community/async-storage';
 import NewsStore from './newsStore';
 import BookmarkStore from './bookmarkStore';
+
+
+
+const hydrate = create({
+  storage: AsyncStorage,
+  jsonify: true
+})
+
 export default class RootStore {
+
+  newsStore = new NewsStore(this);
+  bookmarkStore = new BookmarkStore(this);
   constructor() {
-    this.newsStore = new NewsStore(this);
-    this.bookmarkStore = new BookmarkStore(this);
+    hydrate('@news-app-bookmarks', this.bookmarkStore).then(() => console.log("bookmark store have been hydrated"))
   }
 }
